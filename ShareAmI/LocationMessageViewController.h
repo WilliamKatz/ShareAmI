c7b2f824a8117286e041b4bdb505dfa9f1654ea5
William Katz <wfk.391@gmail.com>
2014-06-10 10:56:08 -0400

    annotation

---
+++ ShareAmI/LocationMessageViewController.h
@@ -0,0 +1,13 @@
+//
+//  LocationMessageViewController.h
+//  ShareAmI
+//
+//  Created by William Katz on 6/9/14.
+//  Copyright (c) 2014 William Katz. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface LocationMessageViewController : UIViewController
+
+@end
---
+++ ShareAmI/LocationMessageViewController.m
@@ -0,0 +1,38 @@
+//
+//  LocationMessageViewController.m
+//  ShareAmI
+//
+//  Created by William Katz on 6/9/14.
+//  Copyright (c) 2014 William Katz. All rights reserved.
+//
+
+#import "LocationMessageViewController.h"
+
+@interface LocationMessageViewController ()
+
+@end
+
+@implementation LocationMessageViewController
+
+- (id)initWithNibName:(NSString *)nibNameOrNil bundle:(NSBundle *)nibBundleOrNil
+{
+    self = [super initWithNibName:nibNameOrNil bundle:nibBundleOrNil];
+    if (self) {
+        // Custom initialization
+    }
+    return self;
+}
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+	// Do any additional setup after loading the view.
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
